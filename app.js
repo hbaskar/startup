dfba6294d02d787db456e444372274493a754d20
Raghav Baskar <haribaskar@hari.local>
2018-07-28 19:50:09 -0500

    test

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.set('view engine', '.hbs');
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger(process.env.NODE_ENV));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
